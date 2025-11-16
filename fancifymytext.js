ce80adeb18aa2a13dfadf1f8d073a3b31b790553
Vanta <52779496+LJVDev@users.noreply.github.com>
2025-11-16 21:30:52 +0000

    added fieldset

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,8 +1,8 @@
 const bigger = document.getElementById("biggerbutton");
 
 function greet() {
-    document.getElementById("biggerbutton").style.width++;
-    document.getElementById("biggerbutton").style.height++;
+    document.getElementById("biggerbutton").style.width = document.getElementById("biggerbutton").style.width + 1;
+    document.getElementById("biggerbutton").style.height = document.getElementById("biggerbutton").style.height + 1;
 }
 
 
