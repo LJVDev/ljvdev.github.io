175279135216202830a07462c4db79029a3817a4
Vanta <52779496+LJVDev@users.noreply.github.com>
2025-11-17 01:12:19 +0000

    function added

--- fancifymytext.js
+++ fancifymytext.js
@@ -21,9 +21,9 @@ function buttonChange(e) {
     }
 
     const dullify = () => {
-        textLabel.style.all = "unset";
-        boringLabel.style.all = "unset";
-        fancyLabel.style.all = "unset";
+        textLabel.style.all = "revert";
+        boringLabel.style.all = "revert";
+        fancyLabel.style.all = "revert";
     }
     // T T      ==   T F
     // F F      ==   T F
